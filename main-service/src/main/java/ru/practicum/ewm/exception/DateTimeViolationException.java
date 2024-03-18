057928bf41ebc792685af6b5104befd86d3bcd52
AndreyAvdoshin <darkxbird@gmail.com>
2024-03-18 21:13:12 +0900

    fix: Изменить параметры доступа полей у ошибок

--- main-service/src/main/java/ru/practicum/ewm/exception/DateTimeViolationException.java
+++ main-service/src/main/java/ru/practicum/ewm/exception/DateTimeViolationException.java
@@ -1,14 +1,15 @@
 package ru.practicum.ewm.exception;
 
+import lombok.Getter;
+
+@Getter
 public class DateTimeViolationException extends RuntimeException {
-    private String field;
+
+    private final String field;
 
     public DateTimeViolationException(String field, String message) {
         super(message);
         this.field = field;
     }
 
-    public String getField() {
-        return field;
-    }
 }
